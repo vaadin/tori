8c427a3a135f91f4674feec43b413f1d7d0784e3
Teemu Pöntelin <tehapo@gmail.com>
2011-11-03 15:08:03 +0200

    View instantiation is now done with Guice to enable dependency injection within views and presenters.

--- webapp/src/main/java/org/vaadin/tori/view/AbstractToriView.java
+++ webapp/src/main/java/org/vaadin/tori/view/AbstractToriView.java
@@ -1,5 +1,7 @@
 package org.vaadin.tori.view;
 
+import javax.inject.Inject;
+
 import org.apache.log4j.Logger;
 import org.vaadin.navigator.Navigator;
 
@@ -15,17 +17,12 @@ public abstract class AbstractToriView<V extends View, P extends Presenter<V>>
 
     protected Logger log = Logger.getLogger(getClass());
 
-    public AbstractToriView() {
-        // Call the init() here as we are not running in a container that would
-        // support the @PostConstruct annotation.
-        init();
-    }
-
     @Override
     public void init(final Navigator navigator, final Application application) {
         if (log.isDebugEnabled()) {
             log.debug("Initializing view: " + getClass().getName());
         }
+        init();
     }
 
     @Override
@@ -38,4 +35,11 @@ public abstract class AbstractToriView<V extends View, P extends Presenter<V>>
         return null;
     }
 
+    @Inject
+    @Override
+    @SuppressWarnings("unchecked")
+    public void setPresenter(final P presenter) {
+        super.setPresenter(presenter);
+        presenter.setView((V) this);
+    }
 }
