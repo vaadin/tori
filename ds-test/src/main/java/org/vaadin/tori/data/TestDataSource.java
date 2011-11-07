643e1e27cdc84151dad60ffb41b9c04533ee061a
Teemu Pöntelin <tehapo@gmail.com>
2011-11-07 16:35:25 +0200

    Introduced command pattern to TestDataSource to avoid repetition of EntityManager handling code.

--- ds-test/src/main/java/org/vaadin/tori/data/TestDataSource.java
+++ ds-test/src/main/java/org/vaadin/tori/data/TestDataSource.java
@@ -23,29 +23,33 @@ public class TestDataSource implements DataSource {
     }
 
     private void initDatabaseWithTestData() {
-        // read the "test-data.sql" file
-        final BufferedReader reader = new BufferedReader(new InputStreamReader(
-                getClass().getClassLoader()
-                        .getResourceAsStream("test-data.sql")));
-        final EntityManager entityManager = PersistenceUtil.createEntityManager();
-        try {
-            String sqlLine;
-            while ((sqlLine = reader.readLine()) != null) {
-                if (!sqlLine.startsWith("#")) {
-                    // not a comment line -> run as native query
-                    runNativeSql(entityManager, sqlLine);
+        executeWithEntityManager(new Command<Void>() {
+            @Override
+            public final Void execute(final EntityManager em) {
+                // read the "test-data.sql" file
+                final BufferedReader reader = new BufferedReader(
+                        new InputStreamReader(getClass().getClassLoader()
+                                .getResourceAsStream("test-data.sql")));
+                try {
+                    String sqlLine;
+                    while ((sqlLine = reader.readLine()) != null) {
+                        if (!sqlLine.startsWith("#")) {
+                            // not a comment line -> run as native query
+                            runNativeSql(em, sqlLine);
+                        }
+                    }
+                } catch (final IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        e.printStackTrace();
+                    }
                 }
+                return null;
             }
-        } catch (final IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                reader.close();
-            } catch (final IOException e) {
-                e.printStackTrace();
-            }
-            entityManager.close();
-        }
+        });
     }
 
     private void runNativeSql(final EntityManager entityManager,
@@ -60,27 +64,26 @@ public class TestDataSource implements DataSource {
     }
 
     private boolean isEmptyDatabase() {
-        final EntityManager em = PersistenceUtil.createEntityManager();
-        try {
-            final TypedQuery<Long> q = em.createQuery(
-                    "select count(c) from Category c", Long.class);
-            final Long categoryCount = q.getSingleResult();
-            return categoryCount == 0;
-        } finally {
-            em.close();
-        }
+        return executeWithEntityManager(new Command<Boolean>() {
+            @Override
+            public final Boolean execute(final EntityManager em) {
+                final TypedQuery<Long> q = em.createQuery(
+                        "select count(c) from Category c", Long.class);
+                final Long categoryCount = q.getSingleResult();
+                return categoryCount == 0;
+            }
+        });
     }
 
     @Override
     public List<Category> getRootCategories() {
-        final EntityManager em = PersistenceUtil.createEntityManager();
-        try {
-            return em.createQuery("select c from Category c", Category.class)
-                    .getResultList();
-        } finally {
-            em.close();
-        }
-
+        return executeWithEntityManager(new Command<List<Category>>() {
+            @Override
+            public final List<Category> execute(final EntityManager em) {
+                return em.createQuery("select c from Category c",
+                        Category.class).getResultList();
+            }
+        });
     }
 
     @Override
@@ -100,22 +103,42 @@ public class TestDataSource implements DataSource {
 
     @Override
     public List<Thread> getThreads(final Category category) {
+        return executeWithEntityManager(new Command<List<Thread>>() {
+            @Override
+            public final List<Thread> execute(final EntityManager em) {
+                return em.createQuery("select t from Thread t", Thread.class)
+                        .getResultList();
+            }
+        });
+    }
+
+    @Override
+    public Category getCategory(final String categoryId) {
+        return executeWithEntityManager(new Command<Category>() {
+            @Override
+            public final Category execute(final EntityManager em) {
+                return em.find(Category.class, Long.parseLong(categoryId));
+            }
+        });
+    }
+
+    /**
+     * Convenience method to execute a {@link Command} with an EntityManager
+     * instance that will always be closed after the execution.
+     * 
+     * @param command
+     * @return
+     */
+    private static <T> T executeWithEntityManager(final Command<T> command) {
         final EntityManager em = PersistenceUtil.createEntityManager();
         try {
-            return em.createQuery("select t from Thread t", Thread.class)
-                    .getResultList();
+            return command.execute(em);
         } finally {
             em.close();
         }
     }
 
-    @Override
-    public Category getCategory(final String categoryId) {
-        final EntityManager em = PersistenceUtil.createEntityManager();
-        try {
-            return em.find(Category.class, Long.parseLong(categoryId));
-        } finally {
-            em.close();
-        }
+    private static interface Command<T> {
+        T execute(EntityManager em);
     }
 }
