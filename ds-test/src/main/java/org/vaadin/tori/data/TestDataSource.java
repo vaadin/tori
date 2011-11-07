94d20ad4eaf42a1f7b0b213445e9ad58570b945c
Teemu Pöntelin <tehapo@gmail.com>
2011-11-07 16:03:22 +0200

    TestDataSource now initializes the database with test data if the database is empty.

--- ds-test/src/main/java/org/vaadin/tori/data/TestDataSource.java
+++ ds-test/src/main/java/org/vaadin/tori/data/TestDataSource.java
@@ -1,6 +1,12 @@
 package org.vaadin.tori.data;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 
 import org.vaadin.tori.data.entity.Category;
 import org.vaadin.tori.data.entity.Thread;
@@ -9,6 +15,56 @@ import org.vaadin.tori.data.util.PersistenceUtil;
 import com.google.common.collect.Lists;
 
 public class TestDataSource implements DataSource {
+
+    public TestDataSource() {
+        if (isEmptyDatabase()) {
+            initDatabaseWithTestData();
+        }
+    }
+
+    private void initDatabaseWithTestData() {
+        // read the "test-data.sql" file
+        final BufferedReader reader = new BufferedReader(new InputStreamReader(
+                getClass().getClassLoader()
+                        .getResourceAsStream("test-data.sql")));
+        final EntityManager entityManager = PersistenceUtil.getEntityManager();
+        try {
+            String sqlLine;
+            while ((sqlLine = reader.readLine()) != null) {
+                if (!sqlLine.startsWith("#")) {
+                    // not a comment line -> run as native query
+                    runNativeSql(entityManager, sqlLine);
+                }
+            }
+        } catch (final IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                reader.close();
+            } catch (final IOException e) {
+                e.printStackTrace();
+            }
+            entityManager.close();
+        }
+    }
+
+    private void runNativeSql(final EntityManager entityManager,
+            final String sql) {
+        entityManager.getTransaction().begin();
+        try {
+            entityManager.createNativeQuery(sql).executeUpdate();
+            entityManager.getTransaction().commit();
+        } catch (final Exception e) {
+            entityManager.getTransaction().rollback();
+        }
+    }
+
+    private boolean isEmptyDatabase() {
+        final TypedQuery<Long> q = PersistenceUtil.getEntityManager()
+                .createQuery("select count(c) from Category c", Long.class);
+        final Long categoryCount = q.getSingleResult();
+        return categoryCount == 0;
+    }
 
     @Override
     public List<Category> getRootCategories() {
--- ds-test/src/main/resources/test-data.sql
+++ ds-test/src/main/resources/test-data.sql
@@ -1,11 +1,15 @@
-## Categories
+#
+# Test data for Tori.
+# SQL clauses cannot span over multiple lines.
+#
+# Categories
 insert into CATEGORY set ID = 1, NAME = "News & Announcements", DESCRIPTION = "All news and announcements.";
 insert into CATEGORY set ID = 2, NAME = "Using the Forums", DESCRIPTION = "Guidelines and tips for using the forums.";
 insert into CATEGORY set ID = 3, NAME = "Vaadin Discussion", DESCRIPTION = "All Vaadin related discussion.";
 insert into CATEGORY set ID = 4, NAME = "Git Discussion", DESCRIPTION = "All Git related discussion.";
 
-## Threads
+# Threads
 insert into THREAD set ID = 1, TOPIC = "Tori alpha 1 released";
 insert into THREAD set ID = 2, TOPIC = "Writing new messages";
 insert into THREAD set ID = 3, TOPIC = "Issues with widgetset compilation";
-insert into THREAD set ID = 4, TOPIC = "What does 'git rebase' actually do??";
+insert into THREAD set ID = 4, TOPIC = "What does 'git rebase' actually do";
