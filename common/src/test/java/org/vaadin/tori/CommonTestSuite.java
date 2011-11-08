07ce1536c00078b1e74572a176b8bc9f40af44b0
Henrik Paul <henrik.paul@gmail.com>
2011-11-08 11:46:29 +0200

    Added a test suite for Common

---
+++ common/src/test/java/org/vaadin/tori/CommonTestSuite.java
@@ -0,0 +1,11 @@
+package org.vaadin.tori;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ ToriUtilTest.class })
+public class CommonTestSuite {
+
+}
