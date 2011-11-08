afd52420ce2d7a0874bcfb4822f4c66e89098364
Henrik Paul <henrik.paul@gmail.com>
2011-11-08 11:43:19 +0200

    A more complete set of tests for ToriUtil.relativeTimeString()

--- common/src/test/java/org/vaadin/tori/ToriUtilTest.java
+++ common/src/test/java/org/vaadin/tori/ToriUtilTest.java
@@ -8,6 +8,15 @@ import org.junit.Test;
 
 public class ToriUtilTest {
     @Test
+    public void relativeTimeStringAMomentAgo() {
+        final Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.MINUTE, -1);
+        cal.add(Calendar.SECOND, 10);
+        assertEquals("a moment ago",
+                ToriUtil.getRelativeTimeString(cal.getTime()));
+    }
+
+    @Test
     public void relativeTimeStringSingleMinuteAgo() {
         final Calendar cal = Calendar.getInstance();
         cal.add(Calendar.MINUTE, -1);
@@ -27,4 +36,82 @@ public class ToriUtilTest {
                 ToriUtil.getRelativeTimeString(cal.getTime()));
     }
 
+    @Test
+    public void relativeTimeStringSingleHourAgo() {
+        final Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.HOUR, -1);
+        assertEquals("1 hour ago",
+                ToriUtil.getRelativeTimeString(cal.getTime()));
+    }
+
+    @Test
+    public void relativeTimeStringMultipleHoursAgo() {
+        final Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.HOUR, -2);
+        assertEquals("2 hours ago",
+                ToriUtil.getRelativeTimeString(cal.getTime()));
+
+        cal.add(Calendar.HOUR, -1);
+        assertEquals("3 hours ago",
+                ToriUtil.getRelativeTimeString(cal.getTime()));
+    }
+
+    @Test
+    public void relativeTimeStringSingleDayAgo() {
+        final Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.DAY_OF_MONTH, -1);
+        assertEquals("1 day ago", ToriUtil.getRelativeTimeString(cal.getTime()));
+    }
+
+    @Test
+    public void relativeTimeStringMultipleDaysAgo() {
+        final Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.DAY_OF_MONTH, -2);
+        assertEquals("2 days ago",
+                ToriUtil.getRelativeTimeString(cal.getTime()));
+
+        cal.add(Calendar.DAY_OF_MONTH, -1);
+        assertEquals("3 days ago",
+                ToriUtil.getRelativeTimeString(cal.getTime()));
+    }
+
+    @Test
+    public void relativeTimeStringSingleMongthAgo() {
+        final Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.MONTH, -1);
+        assertEquals("1 month ago",
+                ToriUtil.getRelativeTimeString(cal.getTime()));
+    }
+
+    @Test
+    public void relativeTimeStringMultipleMonthsAgo() {
+        final Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.MONTH, -2);
+        assertEquals("2 months ago",
+                ToriUtil.getRelativeTimeString(cal.getTime()));
+
+        cal.add(Calendar.DAY_OF_MONTH, -1);
+        assertEquals("3 months ago",
+                ToriUtil.getRelativeTimeString(cal.getTime()));
+    }
+
+    @Test
+    public void relativeTimeStringSingleYearAgo() {
+        final Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.YEAR, -1);
+        assertEquals("1 year ago",
+                ToriUtil.getRelativeTimeString(cal.getTime()));
+    }
+
+    @Test
+    public void relativeTimeStringMultipleYearsAgo() {
+        final Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.YEAR, -2);
+        assertEquals("2 years ago",
+                ToriUtil.getRelativeTimeString(cal.getTime()));
+
+        cal.add(Calendar.YEAR, -1);
+        assertEquals("3 years ago",
+                ToriUtil.getRelativeTimeString(cal.getTime()));
+    }
 }
