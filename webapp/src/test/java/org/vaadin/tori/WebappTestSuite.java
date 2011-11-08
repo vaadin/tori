aa92df3423b5a67fe7a8fc6e8e3130195610a5d3
Teemu Pöntelin <tehapo@gmail.com>
2011-11-08 11:30:15 +0200

    Added a test suite for webapp.

---
+++ webapp/src/test/java/org/vaadin/tori/WebappTestSuite.java
@@ -0,0 +1,12 @@
+package org.vaadin.tori;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+import org.vaadin.tori.category.CategoryPresenterTest;
+
+@RunWith(Suite.class)
+@SuiteClasses({ CategoryPresenterTest.class })
+public class WebappTestSuite {
+
+}
