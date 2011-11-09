8c16cd92fe1761fdac169f605a4268da16b3add8
Teemu Pöntelin <tehapo@gmail.com>
2011-11-09 15:21:22 +0200

    Simplified HeadingLabel to take advantage of the built-in heading styles in reindeer theme.

--- webapp/src/main/java/org/vaadin/tori/component/HeadingLabel.java
+++ webapp/src/main/java/org/vaadin/tori/component/HeadingLabel.java
@@ -9,17 +9,12 @@ import com.vaadin.ui.Label;
 public class HeadingLabel extends Label {
 
     public enum HeadingLevel {
-        H1, H2, H3, H4, H5, H6
+        H1, H2, H3, H4
     }
 
     public HeadingLabel(final String content, final HeadingLevel level) {
-        super();
-        final String headingTag = level.name().toLowerCase();
-        final String headingHtml = String.format("<%s>%s</%s>", headingTag,
-                content, headingTag);
-
-        setContentMode(Label.CONTENT_XHTML);
-        setValue(headingHtml);
+        super(content);
+        addStyleName(level.name().toLowerCase());
     }
 
 }
