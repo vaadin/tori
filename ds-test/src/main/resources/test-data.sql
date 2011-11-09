2bff188750dc2329486f2d4bdd49f12447dcc3ba
Teemu Pöntelin <tehapo@gmail.com>
2011-11-09 13:02:20 +0200

    Added more test data.

--- ds-test/src/main/resources/test-data.sql
+++ ds-test/src/main/resources/test-data.sql
@@ -17,6 +17,10 @@ insert into DISCUSSIONTHREAD set ID = 1, CATEGORY_ID = 5, TOPIC = "Tori alpha 1 
 insert into DISCUSSIONTHREAD set ID = 2, CATEGORY_ID = 2, TOPIC = "Writing new messages";
 insert into DISCUSSIONTHREAD set ID = 3, CATEGORY_ID = 3, TOPIC = "Issues with widgetset compilation";
 insert into DISCUSSIONTHREAD set ID = 4, CATEGORY_ID = 4, TOPIC = "What does 'git rebase' actually do";
+insert into DISCUSSIONTHREAD set ID = 5, CATEGORY_ID = 6, TOPIC = "Announced the future release date of Tori alpha 2";
+insert into DISCUSSIONTHREAD set ID = 6, CATEGORY_ID = 5, TOPIC = "Something new is happening";
+insert into DISCUSSIONTHREAD set ID = 7, CATEGORY_ID = 3, TOPIC = "How to deploy Vaadin applications to Liferay";
+insert into DISCUSSIONTHREAD set ID = 8, CATEGORY_ID = 3, TOPIC = "Issues with widgetset compilation";
 
 ## Users
 insert into `USER` set ID = 1, DISPLAYEDNAME = "John Doe";
@@ -31,3 +35,7 @@ insert into POST set ID = 5, `TIME` = "2011-10-1 12:00:00", THREAD_ID = 3, AUTHO
 insert into POST set ID = 6, `TIME` = "2011-10-2 12:00:00", THREAD_ID = 3, AUTHOR_ID = 2;
 insert into POST set ID = 7, `TIME` = "2011-10-1 12:00:00", THREAD_ID = 4, AUTHOR_ID = 1;
 insert into POST set ID = 8, `TIME` = "2011-10-2 12:00:00", THREAD_ID = 4, AUTHOR_ID = 2;
+insert into POST set ID = 9, `TIME` = "2011-10-4 12:00:00", THREAD_ID = 5, AUTHOR_ID = 2;
+insert into POST set ID = 10, `TIME` = "2011-10-4 12:00:00", THREAD_ID = 6, AUTHOR_ID = 2;
+insert into POST set ID = 11, `TIME` = "2011-10-4 12:00:00", THREAD_ID = 7, AUTHOR_ID = 2;
+insert into POST set ID = 12, `TIME` = "2011-10-5 12:00:00", THREAD_ID = 8, AUTHOR_ID = 2;
