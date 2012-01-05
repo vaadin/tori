426781db82a4aaaeb70a43dc8bce6ebf3868f838
Teemu Pöntelin <tehapo@gmail.com>
2012-01-05 16:17:24 +0200

    Added checks for banned user.

--- ds-liferay/src/main/java/org/vaadin/tori/service/LiferayAuthorizationService.java
+++ ds-liferay/src/main/java/org/vaadin/tori/service/LiferayAuthorizationService.java
@@ -13,17 +13,20 @@ import org.vaadin.tori.service.LiferayAuthorizationConstants.CategoryAction;
 import org.vaadin.tori.service.LiferayAuthorizationConstants.MbAction;
 import org.vaadin.tori.service.LiferayAuthorizationConstants.MessageAction;
 
+import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.security.permission.PermissionChecker;
 import com.liferay.portal.security.permission.PermissionThreadLocal;
 import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portlet.messageboards.service.MBBanLocalServiceUtil;
 
 public class LiferayAuthorizationService implements AuthorizationService,
         PortletRequestAware {
 
     private static final Logger log = Logger
             .getLogger(LiferayAuthorizationService.class);
-    private long scopeGroupId;
+    private long scopeGroupId = -1;
     private String currentUser;
+    private boolean banned;
 
     @Override
     public boolean mayEditCategories() {
@@ -38,9 +41,7 @@ public class LiferayAuthorizationService implements AuthorizationService,
 
     @Override
     public boolean mayReportPosts() {
-        // Liferay doesn't provide any checks for this permission.
-        // Allow for all logged in users.
-        return isLoggedIn();
+        return isLoggedIn() && !isBanned();
     }
 
     @Override
@@ -92,7 +93,7 @@ public class LiferayAuthorizationService implements AuthorizationService,
 
     @Override
     public boolean mayVote() {
-        return isLoggedIn();
+        return isLoggedIn() && !isBanned();
     }
 
     @Override
@@ -141,17 +142,26 @@ public class LiferayAuthorizationService implements AuthorizationService,
 
     private boolean hasCategoryPermission(final CategoryAction action,
             final Category category) {
+        if (isBanned()) {
+            return false;
+        }
         return getPermissionChecker().hasPermission(scopeGroupId,
                 CategoryAction.getScope(), category.getId(), action.toString());
     }
 
     private boolean hasMessagePermission(final MessageAction action,
             final long messageId) {
+        if (isBanned()) {
+            return false;
+        }
         return getPermissionChecker().hasPermission(scopeGroupId,
                 MessageAction.getScope(), messageId, action.toString());
     }
 
     private boolean hasPermission(final MbAction action) {
+        if (isBanned()) {
+            return false;
+        }
         return getPermissionChecker().hasPermission(scopeGroupId,
                 MbAction.getScope(), scopeGroupId, action.toString());
     }
@@ -159,6 +169,10 @@ public class LiferayAuthorizationService implements AuthorizationService,
     private boolean hasMessagePermission(final MessageAction action,
             final Post message) {
         return hasMessagePermission(action, message.getId());
+    }
+
+    private boolean isBanned() {
+        return banned;
     }
 
     private boolean isLoggedIn() {
@@ -174,9 +188,21 @@ public class LiferayAuthorizationService implements AuthorizationService,
         }
     }
 
+    private void setBannedStatus() {
+        // check the ban status and store in the banned field
+        if (currentUser != null) {
+            try {
+                banned = MBBanLocalServiceUtil.hasBan(scopeGroupId,
+                        Long.valueOf(currentUser));
+            } catch (final SystemException e) {
+                log.error("Cannot check ban status for user " + currentUser, e);
+                e.printStackTrace();
+            }
+        }
+    }
+
     @Override
     public void setRequest(final PortletRequest request) {
-        setCurrentUser(request.getRemoteUser());
         if (scopeGroupId < 0) {
             // scope not defined yet -> get if from the request
             final ThemeDisplay themeDisplay = (ThemeDisplay) request
@@ -187,6 +213,9 @@ public class LiferayAuthorizationService implements AuthorizationService,
                 log.info("Using groupId " + scopeGroupId + " as the scope.");
             }
         }
+
+        setCurrentUser(request.getRemoteUser());
+        setBannedStatus();
     }
 
 }
