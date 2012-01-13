a23f69c0ad94d8ccbb682b8b8ecdee743fe68d71
Teemu Pöntelin <tehapo@gmail.com>
2012-01-13 14:02:24 +0200

    Commented out LazyLayout for now because of few final issues with it.

--- webapp/src/main/java/org/vaadin/tori/thread/ThreadViewImpl.java
+++ webapp/src/main/java/org/vaadin/tori/thread/ThreadViewImpl.java
@@ -13,7 +13,6 @@ import org.vaadin.tori.component.FloatingBar.HideEvent;
 import org.vaadin.tori.component.FloatingBar.VisibilityListener;
 import org.vaadin.tori.component.HeadingLabel;
 import org.vaadin.tori.component.HeadingLabel.HeadingLevel;
-import org.vaadin.tori.component.LazyLayout;
 import org.vaadin.tori.component.NewThreadComponent;
 import org.vaadin.tori.component.NewThreadComponent.NewThreadListener;
 import org.vaadin.tori.component.PanicComponent;
@@ -72,15 +71,19 @@ public class ThreadViewImpl extends AbstractView<ThreadView, ThreadPresenter>
     };
 
     private final Map<Post, PostComponent> postsToComponents = new HashMap<Post, PostComponent>();
-    private final LazyLayout postsLayout;
+    // private final LazyLayout postsLayout;
+    private final CssLayout postsLayout;
     private ReplyComponent reply;
 
     public ThreadViewImpl() {
         setStyleName("threadview");
+        postsLayout = new CssLayout();
+        /*-
         postsLayout = new LazyLayout();
         postsLayout.setRenderDistance(RENDER_DISTANCE_PX);
         postsLayout.setPlaceholderSize(PLACEHOLDER_HEIGHT, PLACEHOLDER_WIDTH);
         postsLayout.setRenderDelay(RENDER_DELAY_MILLIS);
+         */
     }
 
     @Override
