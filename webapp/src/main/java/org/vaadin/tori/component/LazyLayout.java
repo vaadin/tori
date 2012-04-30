52d26017b682c057252726c27d7fa6e6aff4cdeb
Henrik Paul <henrik.paul@gmail.com>
2012-04-30 14:05:19 +0300

    eager load after first render works

--- webapp/src/main/java/org/vaadin/tori/component/LazyLayout.java
+++ webapp/src/main/java/org/vaadin/tori/component/LazyLayout.java
@@ -1,5 +1,7 @@
 package org.vaadin.tori.component;
 
+import java.util.Collections;
+
 import org.vaadin.tori.widgetset.client.ui.lazylayout.AbstractLazyLayoutClientRpc;
 import org.vaadin.tori.widgetset.client.ui.lazylayout.AbstractLazyLayoutClientRpc.LazyLayoutClientRpc;
 
@@ -15,5 +17,14 @@ public class LazyLayout extends AbstractLazyLayout {
     public void addComponentEagerly(final Component c) {
         addComponent(c);
         loadedComponents.add(c);
+
+        /*
+         * TODO: this maybe needs to be optimized so that it's not individual
+         * rpc calls, but a queue that gets built and sent over as a state
+         * change. PROBLEM: how do we know when one queue is sent, so that a
+         * new, empty, queue can be built?
+         */
+        getRpcProxy(LazyLayoutClientRpc.class).renderComponents(
+                Collections.singletonList(components.indexOf(c)));
     }
 }
