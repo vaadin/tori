a672fe78d2d0c5a9d22f6cc2b6a79e802c40bb5f
Henrik Paul <henrik.paul@gmail.com>
2012-05-24 13:12:14 +0300

    Separated dynamic binding to another class. Also created a new "Application" for the indexing views

--- webapp/src/main/java/org/vaadin/tori/ToriPortlet.java
+++ webapp/src/main/java/org/vaadin/tori/ToriPortlet.java
@@ -13,55 +13,76 @@ import javax.portlet.PortletResponse;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import com.vaadin.terminal.gwt.server.ApplicationPortlet2;
 
 public class ToriPortlet extends ApplicationPortlet2 {
 
-    private static final String USER_AGENT = "User-Agent";
-    private static final String INDEXER_USER_AGENT_FRAGMENT = "Firefox";
+    private static final String PORTAL_UTIL_CLASS = "com.liferay.portal.util.PortalUtil";
 
     @Override
     protected void handleRequest(final PortletRequest request,
             final PortletResponse response) throws PortletException,
             IOException {
         final HttpServletRequest servletRequest = getServletRequest(request);
+        final HttpServletResponse servletResponse = getServletResponse(response);
         if (servletRequest != null && request instanceof RenderRequest
-                && isIndexerBot(servletRequest)) {
+                && ToriIndexableApplication.isIndexerBot(servletRequest)
+                && ToriIndexableApplication.isIndexableRequest(servletRequest)) {
 
-            printPage(servletRequest, response);
+            final String xhtmlPage = ToriIndexableApplication
+                    .getResultInXhtml(servletRequest);
+
+            final RenderResponse renderResponse = (RenderResponse) response;
+            renderResponse.setContentType("text/html");
+            final OutputStream out = renderResponse.getPortletOutputStream();
+            final PrintWriter outWriter = new PrintWriter(new BufferedWriter(
+                    new OutputStreamWriter(out, "UTF-8")));
+            outWriter.print(xhtmlPage);
+            outWriter.close();
         } else {
             super.handleRequest(request, response);
         }
     }
 
-    private boolean isIndexerBot(final HttpServletRequest servletRequest) {
-        return servletRequest.getHeader(USER_AGENT).contains(
-                INDEXER_USER_AGENT_FRAGMENT);
-    }
-
-    private void printPage(final HttpServletRequest servletRequest,
-            final PortletResponse response) throws IOException {
-        final RenderResponse renderResponse = (RenderResponse) response;
-        renderResponse.setContentType("text/html");
-        final OutputStream out = renderResponse.getPortletOutputStream();
-        final PrintWriter outWriter = new PrintWriter(new BufferedWriter(
-                new OutputStreamWriter(out, "UTF-8")));
-        outWriter.print("<html><body>dummy page</body></html>");
-        outWriter.close();
-    }
-
     private static HttpServletRequest getServletRequest(
             final PortletRequest request) {
         try {
-            final Class<?> portalUtil = Class
-                    .forName("com.liferay.portal.util.PortalUtil");
-            final Method method = portalUtil.getMethod("getHttpServletRequest",
-                    PortletRequest.class);
-            return (HttpServletRequest) method.invoke(null, request);
+            final Class<?> portalUtilClass = Class.forName(PORTAL_UTIL_CLASS);
+
+            /* first get the given servletrequest */
+            final HttpServletRequest httpServletRequest = (HttpServletRequest) portalUtilClass
+                    .getMethod("getHttpServletRequest", PortletRequest.class)
+                    .invoke(null, request);
+
+            /*
+             * but since that's some kind of a fake request, we need the
+             * original one.
+             */
+            final HttpServletRequest originalHttpServletRequest = (HttpServletRequest) portalUtilClass
+                    .getMethod("getOriginalServletRequest",
+                            HttpServletRequest.class).invoke(null,
+                            httpServletRequest);
+
+            return originalHttpServletRequest;
         } catch (final Exception e) {
             e.printStackTrace();
         }
         return null;
     }
+
+    private static HttpServletResponse getServletResponse(
+            final PortletResponse response) {
+        try {
+            final Class<?> portalUtil = Class.forName(PORTAL_UTIL_CLASS);
+            final Method method = portalUtil.getMethod(
+                    "getHttpServletResponse", PortletResponse.class);
+            return (HttpServletResponse) method.invoke(null, response);
+        } catch (final Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 }
