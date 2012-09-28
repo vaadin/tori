74052b064b6b3906e134260319d7a19f24e10183
Henrik Paul <henrik.paul@gmail.com>
2012-09-28 15:06:06 +0300

    Fixed a client-side infinite loop bug

--- webapp/src/main/java/org/vaadin/tori/component/ToriExpandingTextArea.java
+++ webapp/src/main/java/org/vaadin/tori/component/ToriExpandingTextArea.java
@@ -3,27 +3,14 @@ package org.vaadin.tori.component;
 import org.vaadin.hene.expandingtextarea.ExpandingTextArea;
 import org.vaadin.tori.widgetset.client.ui.expandingtextarea.ToriExpandingTextAreaClientRpc;
 
-import com.vaadin.server.PaintException;
-import com.vaadin.server.PaintTarget;
-
 @SuppressWarnings("serial")
 public class ToriExpandingTextArea extends ExpandingTextArea {
-
-    private Integer maxRows = null;
 
     public void blur() {
         getRpcProxy(ToriExpandingTextAreaClientRpc.class).blur();
     }
 
     public void setMaxRows(final int maxRowsExpanded) {
-        maxRows = maxRowsExpanded;
-    }
-
-    @Override
-    public void paintContent(final PaintTarget target) throws PaintException {
-        super.paintContent(target);
-        if (maxRows != null) {
-            target.addAttribute("maxRows", maxRows);
-        }
+        getState().maxRows = maxRowsExpanded;
     }
 }
