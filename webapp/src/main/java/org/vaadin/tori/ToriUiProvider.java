d3dc1a3a722cfdce2029d7a4bf9a63a1b9c08d93
Henrik Paul <henrik.paul@gmail.com>
2012-10-19 09:33:55 +0300

    Fix edit mode to work for Liferay

--- webapp/src/main/java/org/vaadin/tori/ToriUiProvider.java
+++ webapp/src/main/java/org/vaadin/tori/ToriUiProvider.java
@@ -1,22 +1,64 @@
 package org.vaadin.tori;
 
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.EventRequest;
+import javax.portlet.EventResponse;
 import javax.portlet.PortletMode;
-import javax.portlet.PortletRequest;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+import javax.portlet.ResourceRequest;
+import javax.portlet.ResourceResponse;
 
 import org.apache.log4j.Logger;
 
-import com.vaadin.server.CombinedRequest;
 import com.vaadin.server.DefaultUIProvider;
 import com.vaadin.server.UIClassSelectionEvent;
 import com.vaadin.server.UICreateEvent;
-import com.vaadin.server.VaadinPortletRequest;
-import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinPortletSession;
+import com.vaadin.server.VaadinPortletSession.PortletListener;
 import com.vaadin.server.VaadinServiceSession;
 import com.vaadin.ui.UI;
 
 public class ToriUiProvider extends DefaultUIProvider {
 
     private final String themeName;
+
+    @SuppressWarnings("deprecation")
+    class PortletModeListener implements PortletListener {
+        private static final long serialVersionUID = -2798198335357491405L;
+        private final ToriUI toriUi;
+        private PortletMode previousPortletMode;
+
+        public PortletModeListener(final ToriUI toriUi) {
+            this.toriUi = toriUi;
+        }
+
+        @Override
+        public void handleResourceRequest(final ResourceRequest request,
+                final ResourceResponse response, final UI uI) {
+        }
+
+        @Override
+        public void handleRenderRequest(final RenderRequest request,
+                final RenderResponse response, final UI uI) {
+            final PortletMode newPortletMode = request.getPortletMode();
+            if (!newPortletMode.equals(previousPortletMode)) {
+                toriUi.setPortletMode(newPortletMode);
+                previousPortletMode = newPortletMode;
+            }
+        }
+
+        @Override
+        public void handleEventRequest(final EventRequest request,
+                final EventResponse response, final UI uI) {
+        }
+
+        @Override
+        public void handleActionRequest(final ActionRequest request,
+                final ActionResponse response, final UI uI) {
+        }
+    };
 
     public ToriUiProvider(final String themeName) {
         this.themeName = themeName;
@@ -27,17 +69,23 @@ public class ToriUiProvider extends DefaultUIProvider {
         return ToriUI.class;
     }
 
+    @SuppressWarnings("deprecation")
     @Override
     public UI createInstance(final UICreateEvent event) {
+
         final UI ui = super.createInstance(event);
+
         ui.setSession(VaadinServiceSession.getCurrent());
         if (ui instanceof ToriUI) {
             final ToriUI toriUi = (ToriUI) ui;
             toriUi.initApiLoader(event.getRequest());
-            if (shouldRenderEditUI(event.getRequest())) {
-                toriUi.setPortletMode(PortletMode.EDIT);
-            } else {
-                toriUi.setPortletMode(PortletMode.VIEW);
+            final VaadinServiceSession session = VaadinServiceSession
+                    .getCurrent();
+            if (session instanceof VaadinPortletSession) {
+                final VaadinPortletSession portletSession = (VaadinPortletSession) session;
+                portletSession.addPortletListener(new PortletModeListener(
+                        toriUi));
+
             }
         } else {
             Logger.getLogger(getClass()).warn(
@@ -49,12 +97,15 @@ public class ToriUiProvider extends DefaultUIProvider {
         return ui;
     }
 
+    /*-
     private boolean shouldRenderEditUI(final VaadinRequest request) {
         final PortletRequest portletRequest = getPortletRequest(request);
         return portletRequest != null
                 && portletRequest.getPortletMode() == PortletMode.EDIT;
     }
+     */
 
+    /*-
     private PortletRequest getPortletRequest(final VaadinRequest request) {
         if (request instanceof VaadinPortletRequest) {
             return ((VaadinPortletRequest) request).getPortletRequest();
@@ -68,6 +119,7 @@ public class ToriUiProvider extends DefaultUIProvider {
         }
         return null;
     }
+     */
 
     @Override
     public String getTheme(final UICreateEvent event) {
