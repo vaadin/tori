77e2df111e5322b130f16f33d614145f33133872
Henrik Paul <henrik.paul@gmail.com>
2012-10-22 10:33:19 +0300

    Added license headers to source files

--- ds-test/src/main/java/org/vaadin/tori/data/TestDataSource.java
+++ ds-test/src/main/java/org/vaadin/tori/data/TestDataSource.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2012 Vaadin Ltd.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package org.vaadin.tori.data;
 
 import java.io.BufferedReader;
@@ -797,6 +813,7 @@ public class TestDataSource implements DataSource, DebugDataSource {
     }
 
     @Override
+    @edu.umd.cs.findbugs.annotations.SuppressWarnings(value = "DMI_RANDOM_USED_ONLY_ONCE", justification = "just a test implementation")
     public boolean isRead(final DiscussionThread thread) {
         // TODO actual implementation
         return new Random().nextBoolean();
--- webapp/src/main/java/org/vaadin/tori/util/StyleConstants.java
+++ webapp/src/main/java/org/vaadin/tori/util/StyleConstants.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2012 Vaadin Ltd.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package org.vaadin.tori.util;
 
 import com.vaadin.ui.HorizontalLayout;
@@ -19,7 +35,8 @@ public final class StyleConstants {
     public static final String HALF_MARGIN = "halfMargin";
 
     /**
-     * Use for {@link PopupButton} when you want to hide the indicator arrow.
+     * Use for {@link org.vaadin.hene.popupbutton.PopupButton} when you want to
+     * hide the indicator arrow.
      */
     public static final String POPUP_INDICATOR_HIDDEN = "popupIndicatorHidden";
 
