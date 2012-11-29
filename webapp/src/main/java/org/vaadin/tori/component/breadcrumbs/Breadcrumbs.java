c669e900caed7f13536ee33fabcbb24a2fe99420
Henrik Paul <henrik.paul@gmail.com>
2012-11-29 09:37:48 +0200

    Back to where we were, but without splitbuttons and a simpler layout

--- webapp/src/main/java/org/vaadin/tori/component/breadcrumbs/Breadcrumbs.java
+++ webapp/src/main/java/org/vaadin/tori/component/breadcrumbs/Breadcrumbs.java
@@ -16,27 +16,34 @@
 
 package org.vaadin.tori.component.breadcrumbs;
 
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.vaadin.hene.popupbutton.PopupButton;
+import org.vaadin.hene.popupbutton.PopupButton.PopupVisibilityListener;
 import org.vaadin.tori.ToriNavigator;
 import org.vaadin.tori.ToriNavigator.ViewChangeListener;
+import org.vaadin.tori.ToriUI;
 import org.vaadin.tori.category.CategoryView;
-import org.vaadin.tori.component.breadcrumbs.CategoryCrumb.CategorySelectionListener;
-import org.vaadin.tori.component.breadcrumbs.ThreadCrumb.ThreadSelectionListener;
 import org.vaadin.tori.data.entity.Category;
 import org.vaadin.tori.data.entity.DiscussionThread;
+import org.vaadin.tori.exception.DataSourceException;
 import org.vaadin.tori.mvp.View;
 import org.vaadin.tori.thread.ThreadView;
 
+import com.vaadin.event.ItemClickEvent;
 import com.vaadin.server.ExternalResource;
+import com.vaadin.ui.Component;
 import com.vaadin.ui.CustomComponent;
 import com.vaadin.ui.CustomLayout;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.Link;
+import com.vaadin.ui.Tree;
 import com.vaadin.ui.VerticalLayout;
 
 @SuppressWarnings("serial")
-public class Breadcrumbs extends CustomComponent implements
-        CategorySelectionListener, ThreadSelectionListener {
+public class Breadcrumbs extends CustomComponent {
 
     static final String STYLE_CRUMB = "crumb";
     static final String STYLE_THREAD = "thread";
@@ -111,11 +118,7 @@ public class Breadcrumbs extends CustomComponent implements
         final Category currentCategory = threadView.getCurrentCategory();
 
         if (currentThread != null) {
-            final Link categoryCrumb = new Link(currentCategory.getName(),
-                    new ExternalResource("#"
-                            + ToriNavigator.ApplicationView.CATEGORIES.getUrl()
-                            + "/" + currentCategory.getId()));
-            layout.addComponent(categoryCrumb, "category");
+            inputCategoryCrumb(currentCategory, false);
 
             final Link threadCrumb = new Link(currentThread.getTopic(),
                     new ExternalResource("#"
@@ -125,12 +128,7 @@ public class Breadcrumbs extends CustomComponent implements
             layout.addComponent(threadCrumb, "thread");
             showViewCaption(currentThread.getTopic());
         } else if (currentCategory != null) {
-            final Link crumb = new Link(currentCategory.getName(),
-                    new ExternalResource("#"
-                            + ToriNavigator.ApplicationView.CATEGORIES.getUrl()
-                            + "/" + currentCategory.getId()));
-            crumb.addStyleName("last");
-            layout.addComponent(crumb, "category");
+            inputCategoryCrumb(currentCategory, true);
             showViewCaption(currentCategory.getName());
         } else {
             hideViewCaption();
@@ -140,29 +138,116 @@ public class Breadcrumbs extends CustomComponent implements
     private void paint(final CategoryView categoryView) {
         final Category currentCategory = categoryView.getCurrentCategory();
         if (currentCategory != null) {
-            final Link crumb = new Link(currentCategory.getName(),
-                    new ExternalResource("#"
-                            + ToriNavigator.ApplicationView.CATEGORIES.getUrl()
-                            + "/" + currentCategory.getId()));
-            crumb.addStyleName("last");
-            layout.addComponent(crumb, "category");
+            inputCategoryCrumb(currentCategory, true);
             showViewCaption(currentCategory.getName());
         } else {
             hideViewCaption();
         }
     }
 
-    @Override
+    private void inputCategoryCrumb(final Category currentCategory,
+            final boolean isLast) {
+        final Link crumb = new Link(currentCategory.getName(),
+                new ExternalResource("#"
+                        + ToriNavigator.ApplicationView.CATEGORIES.getUrl()
+                        + "/" + currentCategory.getId()));
+
+        final PopupButton categoryPopup = new PopupButton();
+        categoryPopup.addPopupVisibilityListener(new PopupVisibilityListener() {
+            @Override
+            public void popupVisibilityChange(
+                    final org.vaadin.hene.popupbutton.PopupButton.PopupVisibilityEvent event) {
+                if (event.isPopupVisible()) {
+                    categoryPopup.setComponent(getCategoryPopup(
+                            currentCategory, categoryPopup));
+                }
+            }
+        });
+
+        if (isLast) {
+            crumb.addStyleName("last");
+            categoryPopup.addStyleName("last");
+        }
+
+        layout.addComponent(categoryPopup, "categorypopup");
+        layout.addComponent(crumb, "category");
+    }
+
     public void selectCategory(final Category selectedCategory) {
         final String catIdAsString = String.valueOf(selectedCategory.getId());
         navigator.navigateTo(ToriNavigator.ApplicationView.CATEGORIES,
                 catIdAsString);
     }
 
-    @Override
-    public void selectThread(final DiscussionThread selectedThread) {
-        final String threadIdAsString = String.valueOf(selectedThread.getId());
-        navigator.navigateTo(ToriNavigator.ApplicationView.THREADS,
-                threadIdAsString);
+    private Component getCategoryPopup(final Category currentCategory,
+            final PopupButton popupButton) {
+        final Tree tree = new Tree();
+        tree.setImmediate(true);
+
+        try {
+            final List<Category> rootCategories = ToriUI.getCurrent()
+                    .getDataSource().getRootCategories();
+            for (final Category category : rootCategories) {
+                addCategory(tree, category, null);
+            }
+        } catch (final DataSourceException e) {
+            getLogger().error(e);
+            e.printStackTrace();
+            return new Label("Something went wrong :(");
+        }
+
+        tree.addItemClickListener(new ItemClickEvent.ItemClickListener() {
+            @Override
+            public void itemClick(final ItemClickEvent event) {
+                selectCategory((Category) event.getItemId());
+                popupButton.setPopupVisible(false);
+            }
+        });
+
+        highlight(tree, currentCategory);
+
+        return tree;
+    }
+
+    private static void highlight(final Tree tree,
+            final Category currentCategory) {
+        for (final Object itemId : tree.getItemIds()) {
+            if (itemId.equals(currentCategory)) {
+                /* make sure all the parents to this item are expanded */
+                Object parentId = tree.getParent(itemId);
+                while (parentId != null) {
+                    tree.expandItem(parentId);
+                    parentId = tree.getParent(parentId);
+                }
+
+                final String itemCaption = tree.getItemCaption(itemId);
+                tree.setItemCaption(itemId, "> " + itemCaption);
+                return;
+            }
+        }
+    }
+
+    private static void addCategory(final Tree tree, final Category category,
+            final Category parent) throws DataSourceException {
+        tree.addItem(category);
+        tree.setItemCaption(category, category.getName());
+
+        if (parent != null) {
+            tree.setParent(category, parent);
+        }
+
+        final List<Category> subCategories = ToriUI.getCurrent()
+                .getDataSource().getSubCategories(category);
+        if (subCategories.isEmpty()) {
+            tree.setChildrenAllowed(category, false);
+        } else {
+            for (final Category subCategory : subCategories) {
+                addCategory(tree, subCategory, category);
+            }
+        }
+    }
+
+    private static Logger getLogger() {
+        return Logger.getLogger(Breadcrumbs.class);
     }
 }
