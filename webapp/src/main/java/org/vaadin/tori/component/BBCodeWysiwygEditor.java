9195e507f3dcec82b4a80ece9ca72b46bb673979
Tomi Virkki <virkki@vaadin.com>
2014-01-21 10:49:31 +0200

    Text editor updates and cleanup

--- webapp/src/main/java/org/vaadin/tori/component/BBCodeWysiwygEditor.java
+++ webapp/src/main/java/org/vaadin/tori/component/BBCodeWysiwygEditor.java
@@ -18,15 +18,21 @@ package org.vaadin.tori.component;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.vaadin.openesignforms.ckeditor.CKEditorConfig;
 import org.vaadin.openesignforms.ckeditor.CKEditorTextField;
+import org.vaadin.openesignforms.ckeditor.widgetset.client.ui.VCKEditorTextField;
 import org.vaadin.tori.ToriApiLoader;
 import org.vaadin.tori.util.PostFormatter.FontsInfo;
 import org.vaadin.tori.util.PostFormatter.FontsInfo.FontFace;
 import org.vaadin.tori.util.PostFormatter.FontsInfo.FontSize;
 
+import com.vaadin.event.FieldEvents.BlurEvent;
+import com.vaadin.event.FieldEvents.BlurListener;
 import com.vaadin.server.Page;
+import com.vaadin.server.PaintException;
+import com.vaadin.server.PaintTarget;
 import com.vaadin.server.VaadinService;
 import com.vaadin.server.WebBrowser;
 import com.vaadin.ui.UI;
@@ -38,46 +44,37 @@ public class BBCodeWysiwygEditor extends CKEditorTextField {
         addStyleName("wysiwyg-editor");
         setCaption(captionText);
         setHeight(null);
+        setImmediate(true);
 
         CKEditorConfig config = new CKEditorConfig();
+        configureTheme(config);
+        configurePlugins(config, autoGrow);
+        configureFonts(config);
+        configureToolbar(config);
+
+        config.setForcePasteAsPlainText(true);
+        config.disableElementsPath();
+        config.setEnterMode(1);
+
         setConfig(config);
 
-        String themesPath = VaadinService.getCurrentRequest().getContextPath()
-                + "/VAADIN/themes/";
-        String toriTheme = UI.getCurrent().getTheme() + "/";
-        String toriCss = themesPath + toriTheme + "styles.css";
-        String editorCss = themesPath + toriTheme + "editor/editor.css";
-        WebBrowser browser = Page.getCurrent().getWebBrowser();
-        if (browser.isIE()) {
-            String ieCss = themesPath + toriTheme + "editor/ie.css";
-            config.setContentsCss(toriCss, editorCss, ieCss);
-        } else {
-            config.setContentsCss(toriCss, editorCss);
-        }
-        config.setBodyClass("v-app v-widget authoring "
-                + UI.getCurrent().getTheme());
-
-        config.addToExtraPlugins("custombbcode");
-        config.addToExtraPlugins("codebutton");
-
-        boolean disableResizer = true;
-        if (autoGrow) {
-            if (browser.isChrome() || browser.isSafari() || browser.isOpera()) {
-                config.addToExtraPlugins("autogrow");
-            } else {
-                disableResizer = false;
+        addBlurListener(new BlurListener() {
+            @Override
+            public void blur(BlurEvent event) {
+                // Ignore (listener needed by the immediate mode)
             }
-        }
+        });
+    }
 
-        if (disableResizer) {
-            config.disableResizeEditor();
-        }
-
+    private void configureToolbar(CKEditorConfig config) {
         config.addCustomToolbarLine("{ items: ['Font','FontSize'] },"
                 + "{ items: ['Bold','Italic','Underline','Strike','TextColor','RemoveFormat'] },"
                 + "{ items: ['codebutton'] },"
                 + "{ items: ['Link','Image','NumberedList','BulletedList'] },"
                 + "{ items: ['Source'] }");
+    }
+
+    private void configureFonts(CKEditorConfig config) {
         FontsInfo fontsInfo = ToriApiLoader.getCurrent().getPostFormatter()
                 .getFontsInfo();
         List<String> fontNames = new ArrayList<String>();
@@ -85,11 +82,6 @@ public class BBCodeWysiwygEditor extends CKEditorTextField {
             fontNames.add(ff.getFontName());
         }
         config.setFontNames(fontNames);
-        config.setForcePasteAsPlainText(true);
-        config.disableElementsPath();
-        config.addToRemovePlugins("magicline");
-
-        config.setEnterMode(1);
 
         StringBuilder sb = new StringBuilder();
         for (FontSize fs : fontsInfo.getFontSizes()) {
@@ -102,6 +94,63 @@ public class BBCodeWysiwygEditor extends CKEditorTextField {
         sb.append("'").insert(0, "'");
 
         config.addExtraConfig("fontSize_sizes", sb.toString());
+    }
 
+    private void configurePlugins(CKEditorConfig config, boolean autoGrow) {
+        config.addToExtraPlugins("custombbcode");
+        config.addToExtraPlugins("codebutton");
+        config.addToRemovePlugins("magicline");
+
+        WebBrowser browser = Page.getCurrent().getWebBrowser();
+        boolean disableResizer = true;
+        if (autoGrow) {
+            if (browser.isChrome() || browser.isSafari() || browser.isOpera()) {
+                config.addToExtraPlugins("autogrow");
+            } else {
+                disableResizer = false;
+            }
+        }
+
+        if (disableResizer) {
+            config.disableResizeEditor();
+        }
+    }
+
+    private void configureTheme(CKEditorConfig config) {
+        String themesPath = VaadinService.getCurrentRequest().getContextPath()
+                + "/VAADIN/themes/";
+        String toriTheme = UI.getCurrent().getTheme() + "/";
+        String toriCss = themesPath + toriTheme + "styles.css";
+        String editorCss = themesPath + toriTheme + "editor/editor.css";
+
+        WebBrowser browser = Page.getCurrent().getWebBrowser();
+        if (browser.isIE()) {
+            String ieCss = themesPath + toriTheme + "editor/ie.css";
+            config.setContentsCss(toriCss, editorCss, ieCss);
+        } else {
+            config.setContentsCss(toriCss, editorCss);
+        }
+        config.setBodyClass("v-app v-widget authoring "
+                + UI.getCurrent().getTheme());
+    }
+
+    // The following avoids repaints while the user is typing a message.
+    private boolean ignoreNextRepaint;
+
+    @Override
+    public void changeVariables(Object source, Map<String, Object> variables) {
+        if (variables.containsKey(VCKEditorTextField.VAR_TEXT)) {
+            ignoreNextRepaint = true;
+        }
+        super.changeVariables(source, variables);
+    }
+
+    @Override
+    public void paintContent(PaintTarget target) throws PaintException {
+        if (ignoreNextRepaint) {
+            ignoreNextRepaint = false;
+        } else {
+            super.paintContent(target);
+        }
     }
 }
